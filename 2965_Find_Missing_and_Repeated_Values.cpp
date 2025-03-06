595189189053b07a468131c9a3d6dba522c34659
erictsaii <erictsai90@gmail.com>
2025-03-06 22:19:15 +0800

    complete 2965

---
+++ 2965_Find_Missing_and_Repeated_Values.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+            int n = grid.size();
+            vector<int> ans(2);
+            vector<int> v(n * n + 1, 0);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    v[grid[i][j]]++;
+                }
+            }
+            for (int i = 1; i <= n * n; i++) {
+                if (v[i] == 0) ans[1] = i;
+                if (v[i] == 2) ans[0] = i;
+            }
+    
+            return ans;
+        }
+    };
