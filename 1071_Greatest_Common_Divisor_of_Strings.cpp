a4b7a51ef0b2bc3291dec6e340c54fad4cbf18d1
erictsaii <erictsai90@gmail.com>
2025-05-13 22:50:30 +0800

    complete 1071

---
+++ 1071_Greatest_Common_Divisor_of_Strings.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int gcd(int a, int b) {
+        if (a % b == 0) return b;
+        return gcd(b, a % b);
+    }
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1) return "";
+        return str1.substr(0, gcd(str1.size(), str2.size()));
+    }
+};
