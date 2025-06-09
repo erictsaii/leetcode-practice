1b7405f2488764b06e8c3fd8c69f67d1dd48a046
Chih-Tai Tsai <86649653+erictsaii@users.noreply.github.com>
2025-06-09 20:51:21 +0800

    complete 1

---
+++ 1_Two_Sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m; // val, idx
+    
+        for (int i = 0; i < nums.size(); i++) {
+            int remain = target - nums[i];
+            if (m.find(remain) != m.end()) return {m[remain], i};
+            m[nums[i]] = i;
+        }
+        return {};
+    }
+};
