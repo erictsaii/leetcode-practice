cd7fed179298d3911b2cfab0982df48e3e5bfb70
Chih-Tai Tsai <86649653+erictsaii@users.noreply.github.com>
2025-11-04 21:53:00 +0800

    complete 419

---
+++ 419_Battleships_in_a_Board.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int m = board.size(), n = board[0].size();
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'X') {
+                    if ((i==0 || board[i-1][j]=='.') && (j==0 || board[i][j-1]=='.')) ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
