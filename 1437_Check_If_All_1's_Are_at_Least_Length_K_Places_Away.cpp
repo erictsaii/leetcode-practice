aae7ab661c79c3f465f606294a36b9fe95c51df1
Chih-Tai Tsai <86649653+erictsaii@users.noreply.github.com>
2025-11-17 17:11:23 +0800

    complete 1437

---
+++ 1437_Check_If_All_1's_Are_at_Least_Length_K_Places_Away.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int last_one_index = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i]) {
+                if (last_one_index != -1 && i-last_one_index-1 < k) return false;
+                last_one_index = i;
+            }
+        }
+        return true;
+    }
+};
